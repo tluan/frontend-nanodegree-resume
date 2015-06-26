32fbc5273e6ae92ce826abf9badb1bd02005b860
ERICA <kayyet@gmail.com>
2015-06-26 03:32:49 -0400

    1
    1

---
+++ js/resumeBuilder.js
@@ -0,0 +1,5 @@
+$("#main").append("Erica");
+formattedName=HTMLheaderName.replace("%data%","Erica Tu Luan");
+formattedRole=HTMLheaderRole.replace("%data%","Student in a college");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
---
+++ js/untitled.js
@@ -0,0 +1,13 @@
+<p style="line-height: 18px; font-size: 18px;  font-family: times;">
+Click "<i>Load samples</i>" to view and edit more JS samples.<br>
+<br>
+Labyrinth generated with JavaScript:<br><br>
+<script>
+var i=5;
+  do {
+    i++;
+    document.write(i);
+  }while(i<5);
+ 
+</script>
+</p>
